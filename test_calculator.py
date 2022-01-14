c649daef5d94e429d71ae0aa70855c007530428a
Sneha Dinde <sneha.dinde@gmail.com>
2022-01-14 15:43:09 +0530

    replaced find_element_by_xpath with find_element

--- test_calculator.py
+++ test_calculator.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 import time
 import pytest
+from selenium.webdriver.common.by import By
 
 
 @pytest.fixture(scope="module")
@@ -18,66 +19,66 @@ def test_multiplication(setup):
     num2 = 525
     expected_result = 222075
 
-    browser.find_element_by_xpath('/html/body').send_keys(num1)
-    browser.find_element_by_xpath('//div[3]/span[4][@class="sciop"]').click()  #multiplication key
-    browser.find_element_by_xpath('/html/body').send_keys(num2)
-    browser.find_element_by_xpath('//div[5]/span[4][@class="scieq"]').click()
+    browser.find_element(By.XPATH,'/html/body').send_keys(num1)
+    browser.find_element(By.XPATH, '//div[3]/span[4][@class="sciop"]').click()  #multiplication key
+    browser.find_element(By.XPATH,'/html/body').send_keys(num2)
+    browser.find_element(By.XPATH,'//div[5]/span[4][@class="scieq"]').click()
 
     time.sleep(2)
-    actual_result_str= browser.find_element_by_xpath('//div[2][@id="sciOutPut"]').text
+    actual_result_str= browser.find_element(By.XPATH,'//div[2][@id="sciOutPut"]').text
     actual_result = int(actual_result_str)
     assert expected_result == actual_result
 
-    browser.find_element_by_xpath('//div[5]/span[3][@class="scieq"]').click()   #all clear(AC) key
+    browser.find_element(By.XPATH,'//div[5]/span[3][@class="scieq"]').click()   #all clear(AC) key
 
 def test_division(setup):
     num1 = 4000
     num2 = 200
     expected_result = 20
 
-    browser.find_element_by_xpath('/html/body').send_keys(num1)
-    browser.find_element_by_xpath('//div[4]/span[4][@class="sciop"]').click()   #division key
-    browser.find_element_by_xpath('/html/body').send_keys(num2)
-    browser.find_element_by_xpath('//div[5]/span[4][@class="scieq"]').click()
+    browser.find_element(By.XPATH,'/html/body').send_keys(num1)
+    browser.find_element(By.XPATH,'//div[4]/span[4][@class="sciop"]').click()   #division key
+    browser.find_element(By.XPATH,'/html/body').send_keys(num2)
+    browser.find_element(By.XPATH,'//div[5]/span[4][@class="scieq"]').click()
 
     time.sleep(2)
-    actual_result_str = browser.find_element_by_xpath('//div[2][@id="sciOutPut"]').text
+    actual_result_str = browser.find_element(By.XPATH,'//div[2][@id="sciOutPut"]').text
     actual_result = int(actual_result_str)
     assert actual_result == expected_result
 
-    browser.find_element_by_xpath('//div[5]/span[3][@class="scieq"]').click()
+    browser.find_element(By.XPATH,'//div[5]/span[3][@class="scieq"]').click()
 
 def test_addition(setup):
     num1 = -234234
     num2 = 345345
     expected_result = 111111
 
-    browser.find_element_by_xpath('/html/body').send_keys(num1)
-    browser.find_element_by_xpath('//div[1]/span[4][@class="sciop"]').click()  #addition key
-    browser.find_element_by_xpath('/html/body').send_keys(num2)
-    browser.find_element_by_xpath('//div[5]/span[4][@class="scieq"]').click()
+    browser.find_element(By.XPATH,'/html/body').send_keys(num1)
+    browser.find_element(By.XPATH,'//div[1]/span[4][@class="sciop"]').click()  #addition key
+    browser.find_element(By.XPATH,'/html/body').send_keys(num2)
+    browser.find_element(By.XPATH,'//div[5]/span[4][@class="scieq"]').click()
 
     time.sleep(2)
-    actual_result_str = browser.find_element_by_xpath('//div[2][@id="sciOutPut"]').text
+    actual_result_str = browser.find_element(By.XPATH,'//div[2][@id="sciOutPut"]').text
     actual_result = int(actual_result_str)
     assert actual_result == expected_result
 
-    browser.find_element_by_xpath('//div[5]/span[3][@class="scieq"]').click()
+    browser.find_element(By.XPATH,'//div[5]/span[3][@class="scieq"]').click()
 
 def test_subtraction(setup):
     num1 = 234823
     num2 = -23094823
     expected_result = 23329646
 
-    browser.find_element_by_xpath('/html/body').send_keys(num1)
-    browser.find_element_by_xpath('//div[2]/span[4][@class="sciop"]').click()
-    browser.find_element_by_xpath('/html/body').send_keys(num2)
-    browser.find_element_by_xpath('//div[5]/span[4][@class="scieq"]').click()
+    browser.find_element(By.XPATH,'/html/body').send_keys(num1)
+    browser.find_element(By.XPATH,'//div[2]/span[4][@class="sciop"]').click()
+    browser.find_element(By.XPATH,'/html/body').send_keys(num2)
+    browser.find_element(By.XPATH,'//div[5]/span[4][@class="scieq"]').click()
     time.sleep(2)
 
-    actual_result_str = browser.find_element_by_xpath('//div[2][@id="sciOutPut"]').text
+    actual_result_str = browser.find_element(By.XPATH,'//div[2][@id="sciOutPut"]').text
     actual_result = int(actual_result_str)
     assert actual_result == expected_result
 
-    browser.find_element_by_xpath('//div[5]/span[3][@class="scieq"]').click()
+    browser.find_element(By.XPATH,'//div[5]/span[3][@class="scieq"]').click()
 
